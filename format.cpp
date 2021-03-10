a75f85c87029887675196ff5b045a9e90baaca4b
한상현 <luck2901@naver.com>
2021-03-10 23:37:34 +0900

    Update

---
+++ format.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+}
