ef2d696a08a442171a943aa501a9504a5be200ad
한상현 <luck2901@naver.com>
2021-03-10 21:04:58 +0900

    BJ_10164

--- 10164.cpp
+++ 10164.cpp
@@ -39,8 +39,7 @@ int main()
             if (i == 0 && j == 0) dp[i][j] = 1;
             else if (i == 0) dp[i][j] = dp[i][j - 1];
             else if (j == 0) dp[i][j] = dp[i - 1][j];
-            else
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     int result1 = dp[checky][checkx];
 
