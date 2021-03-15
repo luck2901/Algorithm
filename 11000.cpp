3bbe9e2018df6a8b352ab0808f6eeabc50679051
한상현 <luck2901@naver.com>
2021-03-15 23:14:19 +0900

    BJ_11000

---
+++ 11000.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    priority_queue<int, vector<int>, greater<int> > q;
+    vector<pair<int, int> > v;
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int num1, num2;
+        cin >> num1 >> num2;
+        v.push_back(make_pair(num1, num2));
+    }
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        int num1 = v[i].first;
+        int num2 = v[i].second;
+        if (q.empty() || q.top() > num1)
+            q.push(num2);
+        else if (q.top() <= num1)
+        {
+            q.pop();
+            q.push(num2);
+        }
+    }
+    cout << q.size() << endl;
+}
