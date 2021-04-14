e2180c46843dc4f2e7d1ef19c2a46535c6646129
한상현 <luck2901@naver.com>
2021-04-14 19:54:00 +0900

    BOJ_2170

---
+++ 2170.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+#define endl "\n"
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, result = 0;
+    vector<pair<int, int> > v;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        v.push_back(make_pair(start, end));
+    }
+    sort(v.begin(), v.end());
+    int start = v[0].first;
+    int end = v[0].second;
+    for (int i = 1; i < N; i++)
+    {
+        if(v[i].first > end)
+        {
+            result += end - start;
+            start = v[i].first;
+            end = v[i].second;
+        }
+        else if(v[i].first <= end)
+        {
+            end = max(end,v[i].second);
+        }
+    }
+    cout << result + (end-start) << endl;
+}
