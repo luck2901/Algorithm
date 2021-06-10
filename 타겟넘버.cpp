d4d5047f5d290b4f28a0b2763d98969444723aa9
한상현 <luck2901@naver.com>
2021-06-10 11:44:43 +0900

    Programmers_타겟넘버

---
+++ 타겟넘버.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int sum, int depth)
+{
+    if (depth == numbers.size())
+    {
+        if (sum == target)
+            answer++;
+        return;
+    }
+    dfs(numbers, target, sum + numbers[depth], depth + 1);
+    dfs(numbers, target, sum - numbers[depth], depth + 1);
+}
+
+int solution(vector<int> numbers, int target)
+{
+
+    dfs(numbers, target, 0, 0);
+
+    return answer;
+}
